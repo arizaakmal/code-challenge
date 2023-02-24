c6c360d03bf8b9fac4edd7abaa73ef44381ed990
arizaakmal <arizaakmal04@gmail.com>
2023-02-24 10:31:41 +0700

    fix kelipatan 3 atau 5

--- php/kelipatan-3-atau-5/index.php
+++ php/kelipatan-3-atau-5/index.php
@@ -4,7 +4,7 @@ function solution($number)
 {
     $sum = 0;
     for ($i = 1; $i < $number; $i++) {
-        if ($i % 3 == 0 || $i % 5 == 0) {
+        if ($i % 3 === 0 || $i % 5 === 0) {
             $sum += $i;
         }
     }
