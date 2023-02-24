5a7e667b5249f3d2c1b1dde6c9f544f48aff22dd
arizaakmal <arizaakmal04@gmail.com>
2023-02-24 10:30:47 +0700

    add kelipatan 3 atau 5

---
+++ php/kelipatan-3-atau-5/index.php
@@ -0,0 +1,14 @@
+<?php
+
+function solution($number)
+{
+    $sum = 0;
+    for ($i = 1; $i < $number; $i++) {
+        if ($i % 3 == 0 || $i % 5 == 0) {
+            $sum += $i;
+        }
+    }
+    return $sum;
+}
+
+echo solution(1000);
