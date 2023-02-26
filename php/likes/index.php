7220420ed5b33647fb42a8cf29f324e14f0c8298
arizaakmal <arizaakmal04@gmail.com>
2023-02-26 10:23:28 +0700

    fix likes php

--- php/likes/index.php
+++ php/likes/index.php
@@ -7,6 +7,10 @@
 // ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 
 
+
+// Ori Code
+
+/*
 function likes($names)
 {
     // Your code here...
@@ -20,6 +24,24 @@ function likes($names)
         return $names[0] . ", " . $names[1] . " and " . $names[2] . " like this";
     } else if (count($names) > 3) {
         return $names[0] . ", " . $names[1] . " and " .  count($names) - 2 . " others like this";
+    }
+}
+*/
+
+function likes($names)
+{
+    $count = count($names);
+    switch ($count) {
+        case 0:
+            return 'no one likes this';
+        case 1:
+            return $names[0] . ' likes this';
+        case 2:
+            return $names[0] . ' and ' . $names[1] . ' like this';
+        case 3:
+            return $names[0] . ', ' . $names[1] . ' and ' . $names[2] . ' like this';
+        default:
+            return $names[0] . ', ' . $names[1] . ' and ' . ($count - 2) . ' others like this';
     }
 }
 
